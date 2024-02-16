5f21d7a415d0aaf0899aedb20565ef5d0f7b1da4
Bruno Conrado <bruno.conrado@sap.com>
2024-02-16 16:14:36 -0300

    feat: create user service

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
-
+import { UserService } from './user/user.service';
+import { User as UserModel } from '@prisma/client';
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+	constructor(
+		private readonly appService: AppService,
+		private readonly userService: UserService,
+	) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+	@Get()
+	getHello(): string {
+		return this.appService.getHello();
+	}
+
+	@Post('user')
+	async createUser(@Body() userData: { id: number; name: string; level: number }): Promise<UserModel> {
+		return this.userService.createUser(userData);
+	}
 }
