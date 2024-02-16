3cdf0ef4a39d0d5fb8d5e293b68352aef150c775
BConrado <kunrathbruno@outlook.com>
2024-02-16 01:28:47 -0300

    chore: remove script.ts test

--- script.ts
+++ script.ts
@@ -2,35 +2,7 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-async function main() {
-  const user = await prisma.user.create({
-    data: {
-      name: 'Conrado Test',
-      level: 1,
-      class: "SHADOW_MONARCH",
-      stats: {
-        create: 
-          { 
-            healthPoints: 300,
-            manaPoints: 100,
-            fatigue: 0,
-            strength: 1,
-            stamina: 100,
-            agility: 1,                   
-            intelligence: 1,                  
-            perception: 1,                    
-            availablePoints: 1,               
-            PhysicalDmgReductionPorcentage: 0,
-            MagicalDmgReductionPorcentage : 0,
-          }
-      },
-    },
-    include: {
-      stats: true
-    }
-  });
-  console.log(user);
-}
+async function main() {}
 
 main()
   .then(async () => {
