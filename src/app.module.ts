5bd68a32f13c179c5f352676c51a505c9630442b
Bruno Conrado <bruno.conrado@sap.com>
2024-02-16 16:33:20 -0300

    fix: user service module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { UserService } from './user/user.service';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
 	imports: [UserModule, PrismaModule],
 	controllers: [AppController],
-	providers: [AppService],
+	providers: [AppService, UserService, PrismaService],
 })
 export class AppModule {}
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
+import { PrismaClient } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
-  providers: [UserService],
+	providers: [UserService, PrismaClient, PrismaService],
 })
 export class UserModule {}
