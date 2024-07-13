1de7b8644813b2b3bd31c687f6b1c2268bcfa694
BConrado <kunrathbruno@outlook.com>
2024-07-13 11:14:45 -0300

    chore: add get all users

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,7 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 import { UserService } from './user/user.service';
-import { User as UserModel } from '@prisma/client';
+import { Prisma, User as UserModel } from '@prisma/client';
 @Controller()
 export class AppController {
 	constructor(
@@ -15,7 +15,12 @@ export class AppController {
 	}
 
 	@Post('user')
-	async createUser(@Body() userData: { id: number; name: string; level: number }): Promise<UserModel> {
+	async createUser(@Body() userData: Prisma.UserCreateInput): Promise<UserModel> {
 		return this.userService.createUser(userData);
+	}
+
+	@Get('users')
+	async getUsers(): Promise<UserModel[]> {
+		return this.userService.users({});
 	}
 }
